a78d4ec7ff30c68e2453b8bfa4cceb97fd3a8967
Soureesh Varma <soureeshvarma2004@gmail.com>
2025-07-13 14:47:31 +0530

    Fix: Routing basename using PUBLIC_URL for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/StoreContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <BrowserRouter>
-     <StoreContextProvider>
-       <App />
-     </StoreContextProvider>
-   </BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 );
